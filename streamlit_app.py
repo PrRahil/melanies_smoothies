88543e2b4dbc144de2750d13e9ec47fafee1f607
Rahil <129478414+PrRahil@users.noreply.github.com>
2026-01-17 13:18:39 +0000

    Refactor smoothie app to convert Snowflake dataframe to Pandas and display nutrition information for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -15,7 +16,13 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx  = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data = my_dataframe, use_container_width = True)
+# st.dataframe(data = my_dataframe, use_container_width = True)
+# st.stop()
+
+#convert snowflake dataframe to pandas dataframe to use loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ",
@@ -30,6 +37,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
         #heres new section with smoothiefroot, api integration, and snowflake data
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         #st.text(smoothiefroot_response.json())
